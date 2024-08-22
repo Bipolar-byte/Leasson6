db15c30e34f6a24fe231b21941f487c802b8ca7f
Bipolar-byte <Dimanza33@yandex.ru>
2024-08-22 13:25:43 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -5,11 +5,11 @@ print(Basement.get("Ghost"))
 Basement["Ghost"] = 76561198972082941
 Basement.update({"Bunny": 76561198407751342, "Blite": 76561198972082941})
 print(Basement)
-del Basement["Bunny"]
+Basement.pop("Bunny")
 print(Basement)
 Basement_set = {"Leela": 76561198218966161, "LuxMan": 76561198064963183, "Zala": 76561198426131399, "LuxMan": 76561198064963183, "LuxMan": 76561198064963183, "Zala": 76561198426131399}
 print(Basement_set)
 Basement_set.update({"Bunny": 76561198407751342, "Blite": 76561198972082941, "Blite": 76561198972082941, "Bunny": 76561198407751342})
 print(Basement_set)
-del Basement_set["Bunny"]
+Basement_set.pop("Bunny")
 print(Basement_set)
